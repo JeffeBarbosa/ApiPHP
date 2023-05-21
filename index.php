cc5ac9ee0762255e7217d75603a2294490793a36
JeffeBarbosa <62912557+JeffeBarbosa@users.noreply.github.com>
2023-05-21 16:11:50 -0300

    tesate

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 
 require_once "./vendor/autoload.php";
+echo("1");
 require_once "./env.php";
+echo("2");
 require_once "./src/slimConfiguration.php";
+echo("3");
 require_once "./Routes/index.php";
 
 
